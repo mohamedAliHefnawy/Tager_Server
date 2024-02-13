0dd38b9515336f3fa09d8d8cd111f76d0daaf9ba
mohamedAliHefnawy <140818071+mohamedAliHefnawy@users.noreply.github.com>
2024-02-13 14:17:01 -0800

    main

--- routes/store.js
+++ routes/store.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 const route = express.Router();
 const StoresModel = require("../models/store");
+const ProductsModel = require("../models/products");
 
 route.get("/getStores", async (req, res) => {
   try {
@@ -50,6 +51,50 @@ route.post("/editStore", async (req, res) => {
     return res.status(200).send("yes");
   } catch (error) {
     return res.status(500).send("no");
+  }
+});
+
+route.get("/getProductsInStore/:id", async (req, res) => {
+  try {
+    const storeId = req.params.id;
+    const store = await StoresModel.findById(storeId);
+
+    const productIds = store.products.flatMap((product) => product);
+
+    let combinedProducts = [];
+    let combinedProducts2 = [];
+
+    for (const productId of productIds) {
+      const product = await ProductsModel.findById(productId);
+
+      if (product) {
+        combinedProducts2.push(product);
+      } else {
+        const product2 = await ProductsModel.find({
+          products: {
+            $elemMatch: {
+              _id: productId,
+            },
+          },
+        });
+
+        const filteredProducts = product2.map((item) =>
+          item.products.filter((product) =>
+            productIds.includes(product._id.toString())
+          )
+        );
+
+        combinedProducts2 = combinedProducts2.concat(filteredProducts);
+      }
+    }
+
+    const final = combinedProducts2.flatMap((item) => item);
+    const Store = store.gbs;
+    const token = jwt.sign({ final, Store }, config.secretKey);
+    res.json({ token, final, Store });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
