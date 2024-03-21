63e69b83777e56ac6292843108982e3c3b0f6bc4
mohamedAliHefnawy <140818071+mohamedAliHefnawy@users.noreply.github.com>
2024-03-21 14:50:45 -0700

    main

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // elhefnawy:IooPQVqVpZD4nGB8@cluster0.wuy7w3u.mongodb.net/
 mongoose
   .connect(
-    "mongodb+srv://elhefnawy:IooPQVqVpZD4nGB8@cluster0.wuy7w3u.mongodb.net/"
+    "mongodb+srv://elhefnawy:kSUUG9s9UuIiBcEo@cluster0.wuy7w3u.mongodb.net/"
   )
   .then(() => {
     console.log("conected database");
